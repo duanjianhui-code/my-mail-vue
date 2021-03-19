d6766cbf28fa070967f17db93a49e7b1413e99be
duanjianhui <1256591321@qq.com>
2021-03-19 17:45:21 +0800

    首页开发70%

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,60 @@
 <template>
-  <div><h2>首页</h2></div>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <home-recommend-view :recommends="recommends"></home-recommend-view>
+    <feature-views></feature-views>
+  </div>
 </template>
 
 <script>
+import NavBar from "@/components/common/navbar/NavBar";
+import HomeSwiper from "@/views/home/childComps/HomeSwiper";
+import HomeRecommendView from "@/views/home/childComps/HomeRecommendView";
+import FeatureViews from "@/views/home/childComps/FeatureViews";
+
+import {getHomeMultidata} from "@/network/home";
+
+
 export default {
-name: "Home"
+  name: "Home",
+  components: {
+    NavBar,
+    HomeSwiper,
+    HomeRecommendView,
+    FeatureViews
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: []
+    }
+  },
+  created() {
+    //请求数据
+    getHomeMultidata().then(res => {
+     this.banners = res.data.banner.list;
+     this.recommends = res.data.recommend.list;
+
+    })
+  }
+
 }
 </script>
 
 <style scoped>
+#home{
+  padding-top: 44px;
+}
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
+}
 
 </style>
